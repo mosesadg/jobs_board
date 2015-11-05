6b674dccc7128ada8d16c94c38a99c2d7a9e7a75
Anand Moses <moses.adg@gmail.com>
2015-11-05 18:03:40 -0500

    heroku2

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,17 @@
 
 
 Job.destroy_all
+Category.destroy_all
 
 
 # video1 = Video.create(subject:"George Lopez", link_url:"https://www.youtube.com/embed/iPAjcdrbW3c", author:"Anand", date_created:"August 01, 2015", likes:12)
+Cat1 = Category.create(name: "Full Time")
+Cat2 = Category.create(name: "Part Time")
+Cat3 = Category.create(name: "Freelance")
+Cat4 = Category.create(name: "Consulting")
 
-Job1 = Job.create(title: "Test Job", description: "This is description", company: "Microsoft", url: "http://www.google.com", category_id: 4)
+Job1 = Job.create(title: "Test Job", description: "This is description", company: "Microsoft", url: "http://www.google.com", category: Cat1)
+Job2 = Job.create(title: "Test Job", description: "This is description", company: "Facebook", url: "http://www.google.com", category: Cat2)
+Job3 = Job.create(title: "Test Job", description: "This is description", company: "Amazon", url: "http://www.google.com", category: Cat3)
+Job4 = Job.create(title: "Test Job", description: "This is description", company: "Google", url: "http://www.google.com", category: Cat4)
 # <Job id: 10, title: "Test Job", description: "This is description", company: "Microsoft", url: "http://www.google.com", category_id: 4>
