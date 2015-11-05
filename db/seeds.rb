06573c337fba154d55d06f7e03e15b30d6b08144
Anand Moses <moses.adg@gmail.com>
2015-11-05 17:55:21 -0500

    heroku try

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Job.destroy_all
+
+
+# video1 = Video.create(subject:"George Lopez", link_url:"https://www.youtube.com/embed/iPAjcdrbW3c", author:"Anand", date_created:"August 01, 2015", likes:12)
+
+Job1 = Job.create(title: "Test Job", description: "This is description", company: "Microsoft", url: "http://www.google.com", category_id: 4)
+# <Job id: 10, title: "Test Job", description: "This is description", company: "Microsoft", url: "http://www.google.com", category_id: 4>
